1aa2e2cd783d45e52424e6e123b52a597ba1a790
Namikon <namikon@gmx.net>
2015-06-08 08:01:56 +0200

    New versioning-system. No more multi-file-edit for a version update!

--- src/main/java/com/github/namikon/blocklimiter/auxiliary/Reference.java
+++ src/main/java/com/github/namikon/blocklimiter/auxiliary/Reference.java
@@ -7,6 +7,6 @@ package com.github.namikon.blocklimiter.auxiliary;
 public class Reference {
 	public static final String MODID = "blocklimiter";
 	public static final String NAME = "BlockLimiter. Restict your Blocks";
-	public static final String VERSION = "Beta 0.3";
+	public static final String VERSION = "GRADLETOKEN_VERSION";
 	public static final String COLLECTIONNAME = "DreamCraft";
 }
