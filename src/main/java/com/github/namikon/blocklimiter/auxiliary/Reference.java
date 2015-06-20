2c83f96b4553408638a054cc193146fb28f6b19e
Namikon <namikon@gmx.net>
2015-06-20 15:25:11 +0200

    New config-folder name

--- src/main/java/com/github/namikon/blocklimiter/auxiliary/Reference.java
+++ src/main/java/com/github/namikon/blocklimiter/auxiliary/Reference.java
@@ -8,5 +8,5 @@ public class Reference {
 	public static final String MODID = "blocklimiter";
 	public static final String NAME = "BlockLimiter. Restict your Blocks";
 	public static final String VERSION = "GRADLETOKEN_VERSION";
-	public static final String COLLECTIONNAME = "DreamCraft";
+	public static final String COLLECTIONNAME = "GTNewHorizons";
 }
